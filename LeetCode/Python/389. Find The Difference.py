7c4310801da26227c80690e1f75b1ec12cae94dd
Chaitanya Negi <118297902+chaitanyasinghnegi@users.noreply.github.com>
2024-02-02 23:45:56 +0530

    Update 389. Find The Difference.py

--- LeetCode/Python/389. Find The Difference.py
+++ LeetCode/Python/389. Find The Difference.py
@@ -1,6 +1,7 @@
 class Solution:
     def findTheDifference(self, s: str, t: str) -> str:
         for i in set(t):
+#finding the difference between sets
             if t.count(i) != s.count(i):
                 return i
                 break
