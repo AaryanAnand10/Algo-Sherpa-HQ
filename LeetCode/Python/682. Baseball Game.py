4a01429dd9310b0b212adc924cd8983c17757fda
Vatsalwadhi <104412660+Vatsalwadhi@users.noreply.github.com>
2024-02-04 01:34:11 +0530

    Update 682. Baseball Game.py

--- LeetCode/Python/682. Baseball Game.py
+++ LeetCode/Python/682. Baseball Game.py
@@ -4,3 +4,20 @@ class Solution:
         for i in operations:
             result.append(result[len(result)-1]+result[len(result)-2]) if i=='+' else result.append(result[len(result)-1]*2) if i=='D' else result.pop() if i=='C' else result.append(int(i)) 
         return sum(result)
+
+
+
+# alternative solution
+ # class Solution:
+ #    def calPoints(self, operations: List[str]) -> int:
+ #        result=[]
+ #        for i in range(len(operations)):
+ #            if((operations[i]=='D')):
+ #                result.append(result[-1]*2)
+ #            elif(operations[i]=='+'):
+ #                result.append((int(result[-2]))+int(result[-1]))
+ #            elif(operations[i]=='C'):
+ #                result.pop()
+ #            else:
+ #                result.append(int (operations[i]))
+ #        return sum(result)
